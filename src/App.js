9a8b89cec66f0cb61f7fd74cc7ab09dd1520a5bd
Kevin GANEM <Ganemkevin@gmail.com>
2022-01-27 14:57:03 +0100

    trying to fix slider issues

--- src/App.js
+++ src/App.js
@@ -4,29 +4,101 @@ import Box from "./components/Box.js";
 import "./App.css";
 import "./styles/global.css";
 
-const tempMin = -20;
-const tempMax = 40;
-const heartMin = 80;
-const heartMax = 190;
-const stepsMin = 0;
-const stepsMax = 50000;
+const tempMin = -20,
+  tempMax = 40,
+  heartMin = 80,
+  heartMax = 190,
+  stepsMin = 0,
+  stepsMax = 50000;
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.onHeartChange = this.onHeartChange.bind(this);
+    this.changeSteps = this.changeSteps.bind(this);
+    this.changeTemp = this.changeTemp.bind(this);
+
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000,
+    };
+  }
+
+  onHeartChange = (value) => {
+    this.setState({ heart: value });
+  };
+
+  changeSteps = (value) => {
+    this.setState({ steps: value });
+  };
+
+  changeTemp = (value) => {
+    this.setState({ temperature: value });
+  };
+
+  calculWater = (heart, temperature, steps) => {
+    let tempWater = this.state.water;
+
+    if (this.state.temperature > 20) {
+      let diffTemp = (this.state.temperature - 20) * 0.02;
+      tempWater = tempWater + diffTemp;
+    }
+    if (this.state.heart > 120) {
+      let diffHeart = (this.state.heart - 120) * 0.0008;
+      tempWater = tempWater + diffHeart;
+    }
+    if (this.state.steps > 10000) {
+      let diffSteps = (this.state.steps - 10000) * 0.00002;
+      tempWater = tempWater + diffSteps;
+    }
+
+    return tempWater.toFixed(2);
+  };
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           {/* WATER BOX */}
           <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L" />
+
           {/* STEPS BOX */}
-          <Box icon="directions_walk" color="black" value="3000" unit="steps" />
+          <Box
+            icon="directions_walk"
+            color="black"
+            value={this.state.steps}
+            max={stepsMax}
+            min={stepsMin}
+            unit="steps"
+            onChangeVal={this.changeSteps}
+            showSlider="true"
+          />
+
           {/* HEART BOX */}
-          <Box icon="favorite" color="red" value="120" unit="bpm" />
+          <Box
+            icon="favorite"
+            color="red"
+            value={this.state.heart}
+            max={heartMax}
+            min={heartMin}
+            unit="BPM"
+            onChangeVal={this.onHeartChange}
+            showSlider="true"
+          />
+
           {/* TEMPERATURE BOX */}
-          <Box icon="wb_sunny" color="yellow" value="-10" unit="°C" />
-          <p>Heart : {heartMin}</p>
-          <p>Temperature : {tempMin}</p>
-          <p>Steps : {stepsMin}</p>
+          <Box
+            icon="wb_sunny"
+            color="yellow"
+            value={this.state.temperature}
+            max={tempMax}
+            min={tempMin}
+            unit="°C"
+            onChangeVal={this.changeTemp}
+            showSlider="true"
+          />
         </div>
       </div>
     );
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,18 +1,53 @@
 import React from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 class Box extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { needSlider: true };
+  }
+
+  showSlider() {
+    this.setState({ needSlider: true });
+  }
+
+  hideSlider() {
+    this.setState({ needSlider: false });
+  }
+
   render() {
+    const needSlider = this.state.needSlider;
+    let slider;
+    if (needSlider) {
+      slider = (
+        <Slider
+          type="range"
+          min={this.props.min}
+          max={this.props.max}
+          defaultValue={this.props.value}
+          onChange={(value) => this.props.onChangeVal(value)}
+          step="1"
+        />
+      );
+    } else {
+      slider = this.hideSlider();
+    }
     return (
       <div className="box col-sm-3 col-6">
+        {/* ICONES */}
         <span
           className="material-icons"
           style={{ fontSize: "100px", color: this.props.color }}
         >
           {this.props.icon}
         </span>
+        {/* TEXT VALUES */}
         <p>
           {this.props.value} {this.props.unit}
         </p>
+        {slider}
       </div>
     );
   }
